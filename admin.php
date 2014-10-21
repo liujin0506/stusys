ccfde9936fb33b8257948d0fcfbffae3de858a47
liujin0506 <liujin0506@qq.com>
2014-10-21 18:28:16 +0800

    代码提交
    代码提交

---
+++ admin.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_NAME','Admin');
+define('APP_PATH','./Admin/');
+define('THINK_PATH','./ThinkPHP/');
+define('ENGINE_NAME','cluster');
+define('APP_DEBUG',1);
+require THINK_PATH.'ThinkPHP.php';
---
+++ s_admin.php
@@ -0,0 +1,7 @@
+<?php
+define('APP_NAME','Home');
+define('APP_PATH','./Home/');
+define('THINK_PATH','./ThinkPHP/');
+define('ENGINE_NAME','cluster');
+define('APP_DEBUG',1);
+require THINK_PATH.'ThinkPHP.php';
